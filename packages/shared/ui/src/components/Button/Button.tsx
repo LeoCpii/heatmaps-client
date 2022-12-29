5a8e0ce291fb75d5d2bebab78b81d45726531421
leonardo.goncalves <leogoncalves.contato@gmail.com>
2022-12-29 10:15:49 -0300

    feat: create button component

--- packages/shared/ui/src/components/Button/Button.tsx
+++ packages/shared/ui/src/components/Button/Button.tsx
@@ -3,10 +3,35 @@ import { IProps } from './interface';
 
 import './Button.scss';
 
-const Title = ({ children }: IProps) => {
+const Button = ({
+    children,
+    action,
+    theme = 'brand',
+    type = 'button',
+    fluid = false,
+    disabled = false
+}: IProps) => {
+    const cls = () => {
+        const arr = ['ds-btn'];
+
+        if (theme) { arr.push(`_${theme}`); }
+        if (fluid) { arr.push('_fluid'); }
+
+        return arr.join(' ');
+    };
+
+    const click = () => {
+        if (!disabled) { action(); }
+    };
+
     return (
-        <button>{children}</button>
+        <button
+            className={cls()}
+            onClick={() => click()}
+            type={type}
+            disabled={disabled}
+        >{children}</button>
     );
 };
 
-export default Title;
+export default Button;
